9567f3244bc3e8dd5ab8699615d7082656fcc8cb
mansoor88 <mansoor.rafi@hotmail.com>
2021-12-23 15:18:13 +0000

    default controler

---
+++ ShippingManagmeent/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShippingManagmeent.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+
+            string myvar = "test this is string";
+
+
+            return View();
+        }
+    }
+}
